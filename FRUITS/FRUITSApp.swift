5b8af0f13bdc614115ff9d64d894a4681e143591
Selin Çağlar <58820720+selincsc@users.noreply.github.com>
2022-11-17 16:07:57 +0300

    Initial Commit

---
+++ FRUITS/FRUITSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FRUITSApp.swift
+//  FRUITS
+//
+//  Created by Selin Çağlar on 17.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct FRUITSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
