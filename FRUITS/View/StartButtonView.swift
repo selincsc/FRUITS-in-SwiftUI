4a8ac97fbe98e1036d0f23865fab52bf5e311d7f
Selin Çağlar <58820720+selincsc@users.noreply.github.com>
2022-11-23 01:45:16 +0300

    create ListView and RowView

--- FRUITS/View/StartButtonView.swift
+++ FRUITS/View/StartButtonView.swift
@@ -9,10 +9,12 @@ import SwiftUI
 
 struct StartButtonView: View {
     //MARK: - PROPERTY
+    @AppStorage("isOnboarding") var isOnboarding: Bool?
+    
     //MARK: - BODY
     var body: some View {
         Button(action: {
-            print("Exit the onboarding")
+            isOnboarding = false
         }){
             HStack (spacing: 8){
                 Text("Start")
